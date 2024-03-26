ba9d694986032f1310cc3a0df6c29656eb3ef4de
judjdd <jawad.jurin@gmail.com>
2024-03-26 08:19:02 +0300

    register page creation 2

--- lib/Screens/RegisterPage.dart
+++ lib/Screens/RegisterPage.dart
@@ -2,50 +2,118 @@ import 'package:flutter/material.dart';
 import 'package:mawjood_app/cardsWidgets/btnTypes.dart';
 import 'package:mawjood_app/cardsWidgets/button.dart';
 import 'package:mawjood_app/cardsWidgets/iconButton.dart';
-import 'package:mawjood_app/cardsWidgets/infoField.dart';
 
 
 
-class RegistrationPage extends StatelessWidget {
+class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //كنترولز للكتابة
+    TextEditingController nameController = TextEditingController();
+    TextEditingController phoneController = TextEditingController();
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+    TextEditingController jobController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Register'),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            infoField(data: 'Name'),
-            SizedBox(height: 10),
-            infoField(data: 'Phone Number'),
-            SizedBox(height: 10),
-            infoField(data: 'Email'),
-            SizedBox(height: 10),
-            infoField(data: 'Password'),
-            SizedBox(height: 10),
-            infoField(data: 'Job title'),
-            SizedBox(height: 20),
-            const iconButton(
-              text: 'Scan',
-              icon: Icons.camera_alt,
-              type: btnType.Secondary
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: [
+          TextFormField(
+            controller: nameController,
+            decoration: InputDecoration(
+              labelText: 'Name',
+              hintText: 'Your name',
             ),
-            SizedBox(height: 16),
-            const button(
-              text: 'Register',
-              type: btnType.Primary
+          ),
+          SizedBox(height: 8.0),
+          TextFormField(
+            controller: phoneController,
+            decoration: InputDecoration(
+              labelText: 'Phone Number',
+              hintText: 'Your phone number',
             ),
-            SizedBox(height: 20),
-            TextButton(
-              onPressed: () { /* Logic for "Or register with check in" */ },
-              child: Text('Or register with check in'),
+            keyboardType: TextInputType.phone,
+          ),
+          SizedBox(height: 8.0),
+          TextFormField(
+            controller: emailController,
+            decoration: InputDecoration(
+              labelText: 'Email',
+              hintText: 'Your email',
             ),
-          ],
-        ),
+            keyboardType: TextInputType.emailAddress,
+          ),
+          SizedBox(height: 8.0),
+          TextFormField(
+            controller: passwordController,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              hintText: 'Your password',
+            ),
+            obscureText: true,
+          ),
+          SizedBox(height: 8.0),
+          TextFormField(
+            controller: jobController,
+            decoration: InputDecoration(
+              labelText: 'Job title',
+              hintText: 'Type role',
+            ),
+          ),
+          SizedBox(height: 16.0),
+          iconButton( //  icon button for scanning 
+            text: 'Scan',
+            icon: Icons.camera_alt,
+            type: btnType.Secondary, 
+            height: 50,
+            width: double.infinity, 
+          ),
+          SizedBox(height: 16.0),
+          Row(
+            children: [
+              Checkbox(
+                value: false, 
+                onChanged: (bool? newValue) {
+                  // Add the state handling logic later
+                },
+              ),
+              Text('I agree to the terms')
+            ],
+          ),
+          button(
+            text: 'Register',
+            type: btnType.Primary,
+            height: 50,
+            width: double.infinity, 
+          ),
+          SizedBox(height: 16.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Or register with check in'),
+              GestureDetector(
+                onTap: () {
+                  // Add the action here later
+                },
+                child: Text(
+                  'check in ',
+                  style: TextStyle(
+                    color: Theme.of(context).canvasColor, 
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+              ),
+              Icon(Icons.arrow_right, size: 24) //  right arrow icon
+            ],
+          ),
+
+        ],
       ),
     );
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mawjood_app/Screens/RegisterPage.dart';
 import './cardsWidgets/employeeListCard.dart';
 import './cardsWidgets/employeeRequestCard.dart';
 import './cardsWidgets/employeeAttendanceCard.dart';
@@ -15,7 +16,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mawjood',
       home: Scaffold(
         backgroundColor: const Color.fromRGBO(238, 242, 255, 1),
         appBar: AppBar(title: const Text('testing')),
@@ -25,4 +25,3 @@ class MyApp extends StatelessWidget {
   }
 
 }
-
